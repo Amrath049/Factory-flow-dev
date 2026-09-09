b6d323b51013fe478b3df95f83b6e354ac1d157c
amrath <amrathprasadpc@gmail.com>
2026-09-09 21:13:39 +0530

    feat: add UpdateProductDto for product update validation

--- apps/backend/src/products/dto/UpdateProductDto.ts
+++ apps/backend/src/products/dto/UpdateProductDto.ts
@@ -1,4 +1,22 @@
-import { CreateProductDto } from './create-product.dto';
-import { PartialType } from '@nestjs/mapped-types';
+import { IsNumber, IsOptional, IsString, Min } from 'class-validator';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto {
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  price?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  discountedPrice?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  initialStock?: number;
+}
